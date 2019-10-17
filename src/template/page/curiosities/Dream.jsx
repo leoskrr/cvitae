6febb372e48eabe428cd8a7c4989db2541a0821c
leoskrr <47826734+leoskrr@users.noreply.github.com>
2019-10-17 00:19:22 -0300

    Update Dream.jsx

--- src/template/page/curiosities/Dream.jsx
+++ src/template/page/curiosities/Dream.jsx
@@ -7,7 +7,7 @@ export default props=>
             Possuo. O meu maior sonhos é dar uma vida excelente para minha<br/>
             família, além de viajar para os EUA e conhecer a Disney. Gostaria<br/>
             também de conquistar minha independência financeira e viajar muito.<br/>
-            Algo fundamntal que também quero é deixar um legado para esse mundo.<br/>
+            Algo fundamental que também quero é deixar um legado para esse mundo.<br/>
             Não posso esquecer que também gostaria de conhecer minha crush<br/>Jade Picon <i className="fa fa-heart"></i>
 
         </p>
